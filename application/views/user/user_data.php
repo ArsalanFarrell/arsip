40eff60925d521dd891fcf4cec844bfc2d33945b
ArsalanFarrell <follishman99@gmail.com>
2022-09-30 21:35:12 +0700

    first commit

---
+++ application/views/user/user_data.php
@@ -0,0 +1,34 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+    About
+  </h1>
+
+</section>
+
+<!-- Main Content -->
+<section class="content">
+
+  <div class="box">
+    <div class="box-body table-responsive">
+      <div class="class col-md-5">
+        <table class="table" id="table1">
+          <tr>
+          <img src="<?= base_url() ?>assets/images/farrell.jpg" class="img-square" height="250px" alt="User Image">
+          <p>Aplikasi ini dibuat oleh :</p>
+          <p>Nama : Arsalan Farrell Belva Wijaya</p>
+          <p>NIM : 1731710038</p>
+          <p>Tanggal : 26-02-1999</p>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+</section>
+
+<style>
+img {
+  float: left;
+  padding:10px;
+}
+</style>
